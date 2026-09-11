83f21780bf5b47e2d97bf4b7db48bed8426356dc
luscas-rocha <lucas.rocha02@souunit.com.br>
2026-09-11 19:01:27 -0300

    Estruturas Funcionando
    Mudei a maneira que as estruturas sao modeladas. Falta escalar a aparência dos botões para eles não ocuparem a tela inteira

--- jogo/src/gameplay/estrutura.gd
+++ jogo/src/gameplay/estrutura.gd
@@ -1,19 +1,18 @@
 class_name Estrutura
 
-extends Node
+extends Button
 
-var nome : String
-var e_icon : Texture2D
-var custo_inicial : int
+@export var nome : String
+@export var custo_inicial : int
 var custo : int
-var renda : int
-var quantidade : int
+@export var renda : int
+@export var quantidade : int
+
 
 #Construtor
 func _init(c_nome := " ", c_icon := "res://icon.svg", c_custo := 0, c_renda := 0, c_quantidade := 0) -> void:
 	#Carrega um icone para representar a Estrutura
-	e_icon = load(c_icon)
-	
+	icon = load(c_icon)
 	#Construtor Recebe o Custo Inicial apenas
 	custo_inicial = c_custo
 	custo = custo_inicial
@@ -21,20 +20,23 @@ func _init(c_nome := " ", c_icon := "res://icon.svg", c_custo := 0, c_renda := 0
 	nome = c_nome
 	renda = c_renda
 	quantidade = c_quantidade
+	_atualizar_custo()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	get_node("Nome").text = nome
+	custo = custo_inicial
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	get_node("Custo").text = "Custo:" + str(int(custo)) + "Rações"
+	get_node("Quantidade").text = "x" + str(int(quantidade))
+	get_node("Renda").text = str(int(renda)) + "Rações por Segundo"
 
 #Aumenta o custo quando o jogador compra uma estrutura
 func _atualizar_custo() -> void:
-	custo = custo*(1+ (quantidade/100))
+	custo = custo*(1+ (quantidade/10))
 
 #Chamado quando o usuario compra estruturas
 func _comprar(buy_amount :int) -> void:
@@ -50,4 +52,7 @@ func _comprar(buy_amount :int) -> void:
 
 #Gera renda para o jogador
 func _gerar_renda() -> void:
-	GlobalValues.dinheiro += renda
+	GlobalValues.dinheiro += renda*quantidade
+
+func _pressed() -> void:
+	_comprar(1)
--- jogo/src/principal/ui.gd
+++ jogo/src/principal/ui.gd
@@ -1,14 +1,22 @@
 extends Control
 
 @onready var dinheiro := $ColorRect/Dinheiro
-
+@onready var timer := $Timer
+@onready var estruturas = $Estruturas
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	timer.timeout.connect(_on_timeout)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	dinheiro.text = str(int(GlobalValues.dinheiro)) + " rações"
 	pass
+
+func _chamarEstruturas() -> void:
+	for struct in estruturas.get_children():
+		struct._gerar_renda()
+
+func _on_timeout() -> void:
+	_chamarEstruturas()
